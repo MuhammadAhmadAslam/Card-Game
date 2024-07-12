8312661e84e8bcb12629c4ca388f4178bb3c66af
MuhammadAhmadAslam <prince1ahmed111@gmail.com>
2024-07-12 15:16:04 +0500

    Commit

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,7 @@ let backview = document.querySelectorAll('.back-view');
 let clickedCard1 = false;
 let clickedCard2 = false;
 let img = document.querySelectorAll('#img')
-let imagArray = ['assets/images/img1.jpeg','assets/images/img3.jpeg','assets/images/img5.jpeg','assets/images/img2.jpeg','assets/images/img4.jpeg','assets/images/img6.jpeg','assets/images/img1.jpeg','assets/images/img3.jpeg','assets/images/img5.jpeg','assets/images/img2.jpeg','assets/images/img4.jpeg','assets/images/img6.jpeg']
+let imageArray = ['assets/images/img1.jpeg','assets/images/img3.jpeg','assets/images/img5.jpeg','assets/images/img2.jpeg','assets/images/img4.jpeg','assets/images/img6.jpeg','assets/images/img1.jpeg','assets/images/img3.jpeg','assets/images/img5.jpeg','assets/images/img2.jpeg','assets/images/img4.jpeg','assets/images/img6.jpeg']
 card.forEach((card,index)=>{
     card.addEventListener('click' , ()=>{
         forFront()
@@ -27,12 +27,67 @@ function forFront(){
     })
 }
 
+// function distributingImage() {
+//     img.forEach((img,index)=>{
+//         var random = Math.floor(Math.random()*imagArray.length);
+//         img.src = imagArray[random];
+//         console.log(img.src = imagArray[random]);
+//     })
+// }
 function distributingImage() {
-    img.forEach((img,index)=>{
-        var random = Math.floor(Math.random()*imagArray.length);
-        img.src = imagArray[random];
-        console.log(img.src = imagArray[random]);
-    })
+    let imageArray = [
+        'assets/images/img1.jpeg',
+        'assets/images/img2.jpeg',
+        'assets/images/img3.jpeg',
+        'assets/images/img4.jpeg',
+        'assets/images/img5.jpeg',
+        'assets/images/img6.jpeg',
+        'assets/images/img7.jpeg',
+        'assets/images/img8.jpeg'
+      ];
+      
+      // Create a new array with each image duplicated
+      let duplicatedImageArray = [];
+      for (let i = 0; i < imageArray.length; i++) {
+        duplicatedImageArray.push(imageArray[i]);
+        duplicatedImageArray.push(imageArray[i]);
+      }
+      
+      // Shuffle the duplicated array
+      for (let i = duplicatedImageArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = duplicatedImageArray[i];
+        duplicatedImageArray[i] = duplicatedImageArray[j];
+        duplicatedImageArray[j] = temp;
+      }
+      
+      // Get the image elements
+      let img = document.querySelectorAll('#img');
+      
+      // Assign the shuffled images to the image elements
+      for (let i = 0; i < duplicatedImageArray.length; i++) {
+        img[i].src = duplicatedImageArray[i];
+        console.log(img[i].src = duplicatedImageArray[i]);
+      }
+//    // Create a new array with each image duplicated
+// let duplicatedImageArray = [];
+// for (let i = 0; i < imageArray.length; i++) {
+//   duplicatedImageArray.push(imageArray[i]);
+//   duplicatedImageArray.push(imageArray[i]);
+// }
+
+// // Shuffle the duplicated array
+// for (let i = duplicatedImageArray.length - 1; i > 0; i--) {
+//   let j = Math.floor(Math.random() * (i + 1));
+//   let temp = duplicatedImageArray[i];
+//   duplicatedImageArray[i] = duplicatedImageArray[j];
+//   duplicatedImageArray[j] = temp;
+// }
+
+// // Print the shuffled array as cards
+// for (let i = 0; i < duplicatedImageArray.length; i++) {
+//   console.log(`Card ${i + 1}: <img src="${duplicatedImageArray[i]}" />`);
+// }
 }
 
 distributingImage()
